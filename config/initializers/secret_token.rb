79bc7826a9ad63fa368a37ad62786080168c4db5
ukahkhan <your.eukahkhan@gmail.com>
2012-12-26 11:23:28 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ahk::Application.config.secret_token = 'eb7f9033865be742cfc0a3b5469651a6b0071ea3890ab24d265b6e171b40254e7586fb5fbecbe078001eb6cc66784b1e8ab6bc859f85539afdfd4534e29940d5'
